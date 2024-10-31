ccbe07443a106c7ac87729fb6da99c9424ec21c3
blackat <miguel.antonio.martinez.mtz@gmail.com>
2024-10-30 19:33:36 -0600

    primera version

---
+++ map/hefestus.py
@@ -0,0 +1,10 @@
+from mapping import visualizar_stl
+from points import extraer_puntos_unicos
+
+
+
+visualizar_stl('C:/IBERO/TERCER SEMESTRE/Programacion Avanzada/Hefesto/test pieces/pieza estructura.stl')
+puntos_unicos = extraer_puntos_unicos('C:/IBERO/TERCER SEMESTRE/Programacion Avanzada/Hefesto/test pieces/pieza estructura.stl')
+
+#cambios
+#cambios 2
